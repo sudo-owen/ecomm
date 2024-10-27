98f79ffc6ecc6364d58feef31e5dc7bda4d9ea77
owen <22165146+sudo-owen@users.noreply.github.com>
2024-10-27 14:29:38 -0700

    add server side req

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { ProductService } from './services/product.service';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+
+function initializeProductService(productService: ProductService) {
+  return () => new Promise((resolve) => {
+    productService.init().subscribe(success => {
+      resolve(success);
+    });
+  });
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeProductService,
+      multi: true,
+      deps: [ProductService]
+    }
   ],
 };
