a46fb32c8d02bfd3e531d2b9164b626288edc17e
ze-codes <solidman8888@gmail.com>
2024-10-26 17:47:07 -0700

    add landing page, header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { CartDrawerComponent } from './components/cart-drawer/cart-drawer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CartDrawerComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CartDrawerComponent],
+  template: `
+    <div class="flex flex-col min-h-screen">
+      <app-header></app-header>
+      <main class="flex-1">
+        <router-outlet></router-outlet>
+      </main>
+      <app-footer></app-footer>
+    </div>
+    <app-cart-drawer></app-cart-drawer>
+  `
 })
 export class AppComponent {
   title = 'ecomm';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 import { ProductGridComponent } from './components/product-grid/product-grid.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
+  { path: '', component: LandingPageComponent },
   { 
-    path: '', 
+    path: 'products', 
     component: ProductGridComponent 
   },
   { 
-    path: 'product/:id', 
+    path: 'products/:id', 
     component: ProductDetailComponent 
   },
   { 
